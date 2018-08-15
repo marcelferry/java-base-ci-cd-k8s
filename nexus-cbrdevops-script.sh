9fbd7bf9b09977cba0cb1696944ab25bb44b599a
Marcelo Ferreira da Silva <marcelo@mythe.com.br>
2018-08-15 11:37:03 -0300

    Update scripts files

---
+++ nexus-cbrdevops-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Public IP address
+IP="168.61.42.199"
+
+# Name to associate with public IP address
+DNSNAME="nexus-cbrdevops"
+
+# Get resource group and public ip name
+RESOURCEGROUP=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[resourceGroup]" --output tsv)
+PIPNAME=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[name]" --output tsv)
+
+# Update public ip address with dns name
+az network public-ip update --resource-group $RESOURCEGROUP --name  $PIPNAME --dns-name $DNSNAME
